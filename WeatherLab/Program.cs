2c9554f3dde5a3314bbf0f7663c543961f2b3baf
mariasharmagbay <mariasharmagbay@gmail.com>
2021-07-09 23:42:05 -0400

    Completed

--- WeatherLab/Program.cs
+++ WeatherLab/Program.cs
@@ -12,7 +12,7 @@ namespace WeatherLab
         {
             var measurements = new WeatherSqliteContext(dbfile).Weather;
 
-            var total_2020_precipitation = ?? TODO ??
+            var total_2020_precipitation = measurements.Where(w => w.year == 2020).Sum(w => w.precipitation);
             Console.WriteLine($"Total precipitation in 2020: {total_2020_precipitation} mm\n");
 
             //
@@ -21,13 +21,14 @@ namespace WeatherLab
             //
 
             // ?? TODO ??
+            var HeatDegreeDays = measurements.Where(w => w.meantemp < 18);
 
             //
             // Cooling degree days have a mean temp of >=18C
             //
 
             // ?? TODO ??
-
+            var CoolDegreeDays = measurements.Where(w => w.meantemp >= 18);
             //
             // Most Variable days are the days with the biggest temperature
             // range. That is, the largest difference between the maximum and
@@ -43,11 +44,28 @@ namespace WeatherLab
             Console.WriteLine("Year\tHDD\tCDD");
 
             // ?? TODO ??
+            for (var year = 2016; year <= 2020; year++)
+            {
+                var hdd = HeatDegreeDays.Where(n => n.year == year);
+                var cdd = CoolDegreeDays.Where(n => n.year == year);
+                Console.WriteLine($"{year}\t{hdd.Count():d3}\t{cdd.Count():d3}");
+            }
 
             Console.WriteLine("\nTop 5 Most Variable Days");
             Console.WriteLine("YYYY-MM-DD\tDelta");
 
             // ?? TODO ??
+         
+            var varDay = measurements.OrderByDescending(w => (w.maxtemp - w.mintemp));
+            var only = 5;
+            var Delta = varDay.Take(only).Select(w => (w.maxtemp - w.mintemp)).ToArray(); ;
+            var yr = varDay.Take(only).Select(w => w.year).ToArray();
+            var mo = varDay.Take(only).Select(w => w.month).ToArray(); ;
+            var dy = varDay.Take(only).Select(w => w.day).ToArray(); ;
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{yr[i]:d4}-{mo[i]:d2}-{dy[i]:d2}\t {Delta[i]:f5} ");
+            }
         }
     }
 }
